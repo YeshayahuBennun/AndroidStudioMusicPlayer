8c7253b9e5c9e644c3842e0bbf9c51f79acf3af2
YeshayahuBennun <ybennun@yahoo.com>
2021-11-02 20:48:17 +0200

    commit#04 - Music Player - Add Permissions

--- app/src/main/java/com/ybennun/musicplayer/MusicPlayerActivity.kt
+++ app/src/main/java/com/ybennun/musicplayer/MusicPlayerActivity.kt
@@ -1,11 +1,67 @@
 package com.ybennun.musicplayer
 
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 class MusicPlayerActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
+
+        if (Build.VERSION.SDK_INT >= 23)
+            checkPermissions()
+    }
+
+    private fun checkPermissions() {
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            //Read the songs
+        } else {
+            //false -> user asked not to ask me any more/permission disabled
+            //true  -> rejected before want to use the feature again
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                Toast.makeText(this, "Music player needs Access to your files", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_CODE_READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_READ_EXTERNAL_STORAGE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // read songs
+            } else {
+                Toast.makeText(this, "Permission is not Granted", Toast.LENGTH_SHORT).show()
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
     }
 }
